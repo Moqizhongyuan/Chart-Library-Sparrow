d3041fc5034e7fa50d41b9be315d77ea7a1f5761
Moqizhongyuan <2356373295@qq.com>
2024-11-07 17:26:19 +0800

    ✨新增(javascript): 创建render模块，图形模块，transform模块

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,19 @@ export default [
         ...globals.node
       }
     },
-    plugins: { jest: pluginJest }
+    plugins: { jest: pluginJest },
+    rules: {
+      // 关闭 eslint 的如下功能
+      'import/prefer-default-export': 0,
+      'no-use-before-define': 0,
+      'no-shadow': 0,
+      'no-restricted-syntax': 0,
+      'no-return-assign': 0,
+      'no-param-reassign': 0,
+      'no-sequences': 0,
+      'no-loop-func': 0,
+      'no-nested-ternary': 0
+    }
   },
   pluginJs.configs.recommended
 ]
